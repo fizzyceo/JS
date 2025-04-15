1f84f8f769e9696c2ed228e234a1d729787da69a
fizzyceo <ilyesmaro5@gmail.com>
2025-04-15 13:49:23 +0100

    FAQs

---
+++ FAQ/script.js
@@ -0,0 +1,9 @@
+//Select all elements that have the class "element"
+
+//iterate each element and select his button and his description
+
+//logic: if we click on the button of that element we will check if the description is visible or not
+
+//if visible we will hide it and change the button image to "plus.png"
+
+//if not visible we will show it and change the button image to "minus.png"
